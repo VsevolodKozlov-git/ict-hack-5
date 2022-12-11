a4bf9b03ded65dc7380e3180ee50b73d3149747f
Vsevolod Kozlov <vsevolod026@gmail.com>
2022-12-11 16:04:21 +0300

    Squashed commit of the following:
    commit 578f2cf7aa30319ad1c49f68320d0dbb08d81d71
    Author: Vsevolod Kozlov <vsevolod026@gmail.com>
    Date:   Sun Dec 11 16:03:39 2022 +0300
    
        small_change
    
    commit 554ee678c800b1d5dbf93218716021be6b5bd7f1
    Author: Vsevolod Kozlov <vsevolod026@gmail.com>
    Date:   Sat Dec 10 20:42:39 2022 +0300
    
        Оно работает. Нет страницы проверки email

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -35,13 +35,42 @@ class User(db.Model):
     about = db.Column(db.Text, nullable=True)
     growth_area = db.Column(db.Text, nullable=True)
     tags = db.Column(db.Text, nullable=False)
-    # projects_links = db.Column(db.Text)
     education = db.Column(db.Text, nullable=True)
     courses = db.Column(db.Text, nullable=True)
     projects =db.Column(db.Text, nullable=True)
     job_experience = db.Column(db.Text, nullable=True)
     hard_skills = db.Column(db.Text, nullable=True)
     soft_skills = db.Column(db.Text, nullable=True)
+
+
+def validate_login(form, fields):
+    login = fields.data
+    if is_login_in_db(login):
+        raise ValidationError('Логин занят')
+
+def is_login_in_db(login):
+    return User.query.filter_by(login=login).first() is not None
+class RegisterForm(FlaskForm):
+    login = StringField('Логин', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    patronymic = StringField('Отчество')
+    email = StringField('Email', validators=[DataRequired()])
+    contact1_name = StringField('Название контакта 1')
+    contact1_value = StringField('Значение контакта 1')
+    contact2_name = StringField('Название контакта 2')
+    contact2_value = StringField('Значение контакта 3')
+    contact3_name = StringField('Название контакта 3')
+    contact3_value = StringField('Значение контакта 3')
+    about = TextAreaField('Обо мне')
+    growth_area = TextAreaField('Области развития')
+    tags = TextAreaField('Тэги')
+    education = TextAreaField('Образование')
+    courses = TextAreaField('Пройденные курсы')
+    projects = TextAreaField('Проекты')
+    job_experience = TextAreaField('Опыт работы')
+    hard_skills = TextAreaField('Hard skills')
+    soft_skills = TextAreaField('Soft skills')
 
 
 @app.route('/')
@@ -57,6 +86,27 @@ def user_page(login):
         del user_dict['_sa_instance_state']
         return render_template('privatecab.html', **user_dict)
 
+
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('regist.html', form=form)
+
+
+@app.route('/register_post', methods=['GET', 'POST'])
+def handle_register_post():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        if is_login_in_db(form.login.data):
+            return render_template('login_taken.html')
+
+        db_dict = {f.name: f.data for f in form if f.name != 'csrf_token'}
+        new_user = User(**db_dict)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('user_page', login=new_user.login))
 
 
 if __name__ == '__main__':
