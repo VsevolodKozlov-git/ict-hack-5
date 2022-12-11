a3101d40d41c34bb389d7a3679e04b5d4932059c
Vsevolod Kozlov <vsevolod026@gmail.com>
2022-12-11 15:49:58 +0300

    Squashed commit of the following:
    commit daac6524a2761da51c2479c2039eec2bdde073a0
    Author: Vsevolod Kozlov <vsevolod026@gmail.com>
    Date:   Sun Dec 11 15:46:52 2022 +0300
    
        changed html
    
    commit 88996b1e99626369bb228941cc2c81545cd3d63c
    Author: Vsevolod Kozlov <vsevolod026@gmail.com>
    Date:   Sun Dec 11 15:46:38 2022 +0300
    
        changed app
    
    commit 71915dd67fdd2ed919219c60e661d9267a20a3ef
    Author: AliceGuliaeva <you@example.com>
    Date:   Sun Dec 11 15:27:28 2022 +0300
    
        Не знаю что я тут делала
    
    commit f12e67085c72298e2c6ceb1bf8b2df1927334f03
    Author: AliceGuliaeva <you@example.com>
    Date:   Sat Dec 10 20:40:38 2022 +0300
    
        Изменение профиля

--- create_db_entry.py
+++ create_db_entry.py
@@ -2,27 +2,26 @@
 from app import User
 
 
+
 def create_db_entry():
-    db_entry = User(login='anna228',
-         surname='Kozlova',
-         name='Anna',
-         patronymic='Denisovna',
-         email = 'vsevolod026@gmail.com',
+    db_entry = User(login='anton_2002',
+         surname='Иванов',
+         name='Иван',
+         patronymic='Иваныч',
+         email = 'something@gmail.com',
          contact1_name = 'Vk',
-         contact1_value = 'vk_link',
+         contact1_value = 'vk.com/id2134893',
          contact2_name = 'tg',
          contact2_value = '@sample_text',
-         contact3_name = 'whatsapp',
-         contact3_value = '+7...',
-         about = 'something about me and my life',
-         growth_area = 'programming, datascience',
+         about = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+         growth_area = 'Программирование, Искусство',
          tags = 'Cool tag',
          education = 'Баклавр колбасных изделий в Томском тракторном институте',
-         courses = 'Coursera DS, Stepik neuro application',
-         projects ='Проект по разработке приколов\n Проект по питону',
+         courses = 'Coursera "Programming for Everobody", Stepik "Программирование на python"',
+         projects ='Generation digits from mnist dataser',
          # job_experience = db.Column(db.Text, nullable=True)
-         hard_skills = 'Программирование',
-         soft_skills = 'Отсутствуют'
+         hard_skills = 'Программирование, АиДС, математика',
+         soft_skills = 'Коммуникабельность'
     )
     return db_entry
 
